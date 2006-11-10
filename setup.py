c5e2f58aab232656a1acdf826c372091b4d3d43d
llimllib <llimllib@ccd72bc5-f4fc-0310-a744-bb185048b8e6>
2006-11-10 13:40:30 +0000

    Reorg foot.tmpl
    git-svn-id: http://billmill.org/svn/cherry_blossom/trunk@96 ccd72bc5-f4fc-0310-a744-bb185048b8e6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ template_list = glob.glob(os.path.join('templates', '*.tmpl'))
 
 setup(name='cherry_blossom', 
         version='1.0', 
-        description='A blog server like pybloxsom, but written in cherrypy',
+        description='A blog server written in cherrypy',
         author='Bill Mill',
         author_email='bill.mill@gmail.com',
         url='billmill.org',
--- utils.py
+++ utils.py
@@ -2,3 +2,5 @@ import cherrypy as cpy
 
 def config(key, default=None, path='cherryblossom'):
     return cpy.config.get(key, default, path=path)
+
+configMap = cpy.config.configMap
