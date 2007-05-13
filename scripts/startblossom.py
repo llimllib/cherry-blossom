def066a2f1b8c859d8769cfed960397b6d0a607d
llimllib <llimllib@ccd72bc5-f4fc-0310-a744-bb185048b8e6>
2007-05-13 19:10:20 +0000

    Getting a template in place for a blossom startup script
    git-svn-id: http://billmill.org/svn/cherry_blossom/trunk@159 ccd72bc5-f4fc-0310-a744-bb185048b8e6

---
+++ scripts/startblossom.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#(distutils will automatically convert the first line to point to the right 
+#python)
+#
+# Start a blossom server
+
+#how do I figure out what directory cherry blossom is installed to?
--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(name='cherry_blossom',
         description='A blog server written in cherrypy',
         author='Bill Mill',
         author_email='bill.mill@gmail.com',
-        url='billmill.org',
+        url='billmill.org:9561/cherry_blossom',
         py_modules=['Entry', 'FileCabinet', 'blox'],
         packages=['plugins'],
         include_package_data = True,
         license="Public Domain",
+        requires=["cherrypy>=3.0"]
       )
