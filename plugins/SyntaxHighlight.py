8c6134c3a0402db967d7ebba8ba02f4a66842858
llimllib <llimllib@ccd72bc5-f4fc-0310-a744-bb185048b8e6>
2007-05-26 15:25:34 +0000

    A few improvements to SyntaxHighlighter
    git-svn-id: http://billmill.org/svn/cherry_blossom/trunk@170 ccd72bc5-f4fc-0310-a744-bb185048b8e6

--- plugins/SyntaxHighlight.py
+++ plugins/SyntaxHighlight.py
@@ -23,12 +23,15 @@ class SyntaxHighlight(object):
     def cb_story_start(self, entries):
         self.render_css = True
 
-    #story is an Entry
+    #story is an Entry object
     def cb_story(self, story):
         for lang, code in CODE_RE.findall(story._text):
             if not lang: lang = "python"
 
-            lexer = get_lexer_by_name(lang)
+            try:
+                lexer = get_lexer_by_name(lang.strip('"'))
+            except ClassNotFound:
+                return
             formatter = HtmlFormatter(style=config("syntax_style", "default", 
                 "SyntaxHighlight"))
             code = pygments.highlight(code, lexer, formatter)
@@ -36,7 +39,5 @@ class SyntaxHighlight(object):
             if self.render_css:
                 code = '<style type="text/css">%s</style>\n%s' % \
                     (formatter.get_style_defs(), code)
-                story._text = CODE_RE.sub(story._text, code, 1)
                 self.render_css = False
-            else:
-                story._text = CODE_RE.sub(story._text, code, 1)
+            story._text = CODE_RE.sub(code, story._text, 1)
