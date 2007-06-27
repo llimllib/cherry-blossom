9db6ee580a4451b8fe3258bc93160d72afc25252
llimllib <llimllib@ccd72bc5-f4fc-0310-a744-bb185048b8e6>
2007-06-27 03:11:03 +0000

    Removing on-the-fly generation of CSS; I use it frequently enough, and its small enough, to warrant inclusion all the time. #3
    git-svn-id: http://billmill.org/svn/cherry_blossom/trunk@198 ccd72bc5-f4fc-0310-a744-bb185048b8e6

--- plugins/SyntaxHighlight.py
+++ plugins/SyntaxHighlight.py
@@ -1,12 +1,21 @@
 """Prerequisites: pygments (http://pygments.org)
 
+To get code highlighting, you'll need to generate a CSS file from your
+favorite Pygments theme and include it in your header; see an example css
+file at http://billmill.org:9561/cherry_blossom/browser/trunk/static/code.css
+. If you want to generate the CSS on the fly, check out this earlier revision
+of SyntaxHighlighter which did so:
+http://billmill.org:9561/cherry_blossom/browser/trunk/plugins/SyntaxHighlight.py?rev=197
+
+
 Configuration for SyntaxHighlight module should be in a [SyntaxHighlight] 
     secion of your cherryblossom.conf file.
 
 options:
 syntax_style (string): Style to use for highlighting text. Check out the style 
     documentation at http://pygments.org/docs/styles/ and take the styles out 
-    for a test drive at http://pygments.org/demo/"""
+    for a test drive at http://pygments.org/demo/
+"""
 
 import cherrypy as cpy
 import re
@@ -19,13 +28,7 @@ from utils import config
 CODE_RE = re.compile("<code (?:\s*lang=(.*?))*>(.*?)<(?#)/code>", re.S)
 
 class SyntaxHighlight(object):
-    def __init__(self, parent):
-        #so that if cb_feed_render_story gets called before _story_start,
-        #we don't throw an error
-        self.render_css = False
-
-    def cb_story_start(self, entries):
-        self.render_css = True
+    def __init__(self, parent): pass
 
     def highlight_code(self, textstr, font_tags=False):
         for lang, code in CODE_RE.findall(textstr):
@@ -39,10 +42,6 @@ class SyntaxHighlight(object):
                 "SyntaxHighlight"), noclasses=font_tags)
             code = pygments.highlight(code, lexer, formatter)
 
-            if self.render_css:
-                code = '<style type="text/css">%s</style>\n%s' % \
-                    (formatter.get_style_defs(), code)
-                self.render_css = False
             textstr = CODE_RE.sub(code, textstr, 1)
         return textstr
 
@@ -50,7 +49,7 @@ class SyntaxHighlight(object):
     def cb_feed_story(self, story):
         #de-lazify the object
         story.text
-        story._text = self.highlight_code(story._text, True)
+        story._text = self.highlight_code(story._text, font_tags=True)
         
     #story is an Entry object
     def cb_story(self, story):
