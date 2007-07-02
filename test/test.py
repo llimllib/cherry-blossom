fa25bd6a20e1b941144254cf9c70e9746b25f620
llimllib <llimllib@ccd72bc5-f4fc-0310-a744-bb185048b8e6>
2007-07-02 06:50:45 +0000

    Some work on testing, including super-secret work on twill :)
    git-svn-id: http://billmill.org/svn/cherry_blossom/trunk@202 ccd72bc5-f4fc-0310-a744-bb185048b8e6

--- test/test.py
+++ test/test.py
@@ -12,6 +12,7 @@ def setup():
     cpy.config.update("site.conf")
     cpy.config.update("basic.conf")
     wsgi = cpy.tree.mount(blox.BlogRoot(), "/", "basic.conf")
+    wsgi = cpy.tree.mount(blox.BlogRoot(), "/", "basic.conf")
     twill.add_wsgi_intercept('localhost', 4444, lambda: wsgi)
     syserr, sys.stderr = sys.stderr, StringIO()
     try:
@@ -21,13 +22,33 @@ def setup():
 
 def testIndex():
     test = """
+    extend_with header
     debug commands 1
 
     go /
     code 200
+    header charset=utf-8
     """
     twill.execute_string(test, initial_url='http://localhost:4444')
+
+def testi18n():
+    test = """
+    debug commands 1
+
+    go /test_i18n.html
+    code 200
+    find 'I\xc3\xb1t\xc3\xabrn\xc3\xa2ti\xc3\xb4n\xc3\xa0liz\xc3\xa6ti\xc3\xb8n'
+    """
+    twill.execute_string(test, initial_url='http://localhost:4444')
+    browser = twill.get_browser()
+    print dir(browser)
 
 def teardown(module):
     twill.remove_wsgi_intercept('localhost', 4444)
     cpy.engine.stop()
+
+if __name__ == "__main__":
+    #start the server so I can see what it looks like
+    cpy.config.update("site.conf")
+    cpy.config.update("basic.conf")
+    cpy.quickstart(blox.BlogRoot(), "/", "basic.conf")
